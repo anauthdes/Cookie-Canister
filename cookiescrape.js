df5bfc7d98a3c06ee844fb6eaa2fb85a3180a93f
anauthdes <63677547+anauthdes@users.noreply.github.com>
2021-02-15 00:56:24 -0500

    added some filtering for the domains being pulled by webrequest
    -Added filtering of web requests into an array of domains
    -added a domain name cleaner function
    -added a loop to filter through the cookies based on domain

--- cookiescrape.js
+++ cookiescrape.js
@@ -10,36 +10,56 @@
 
 var curTab = 0;
 var IncomingRequests = [];
-
-chrome.webRequest.onBeforeRequest.addListener(function(details){
-	IncomingRequests.push(details);
-	/*if(details.tabId == curTab){
-		console.log(details);
-	}*/
-	return null;
-},{urls: ["<all_urls>"]})
-
-
-chrome.browserAction.onClicked.addListener(function(tab) { 
-	curTab = tab.id;
-	console.log("tabs: ",chrome.tabs);
-	console.log("current tab: ",tab);
-	getAllCookiesFromTabs(tab)
-	console.log(filterRequestsByTabID(IncomingRequests,curTab));
+var matchedCookies = [];
+var filteredDomains = [];
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    IncomingRequests.push(details);
+    /*if(details.tabId == curTab){
+    	console.log(details);
+    }*/
+    return null;
+}, { urls: ["<all_urls>"] })
 
 
+chrome.browserAction.onClicked.addListener(function(tab) {
+    curTab = tab.id;
+    console.log("tabs: ", chrome.tabs);
+    console.log("current tab: ", tab);
+    // getAllCookiesFromTabs(tab)
+    console.log("filtered requests", filterRequestsByTabID(IncomingRequests, curTab));
+    filteredDomains = getInitiatorDomains(filterRequestsByTabID(IncomingRequests, curTab))
+    for (var iii = 0; iii < filteredDomains.length; iii++) {
+        chrome.cookies.getAll({domain:filteredDomains[iii]}, function(cookies) {
+            console.log("filtered cookies (",filteredDomains[iii],") :",cookies);
+        });
+    }
 
 });
 
 
-function getAllCookiesFromTabs(tabs){
-	chrome.cookies.getAll({},function(cookies){
-		console.log(cookies);
-	});
+function getAllCookiesFromTabs(tabs) {
+    chrome.cookies.getAll({}, function(cookies) {
+        console.log(cookies);
+    });
 }
 
-function filterRequestsByTabID(requests, tabID){
-	return requests.filter(function (el) {
-  return el.tabId == tabID;
-});
+function filterRequestsByTabID(requests, tabID) {
+    return requests.filter(function(el) {
+        return el.tabId == tabID;
+    });
+}
+
+function getInitiatorDomains(requests) {
+    var domains = [];
+    for (var iii = 0; iii < requests.length; iii++) {
+        if (domains.indexOf(cleanDomain(requests[iii].initiator)) < 0) {
+            domains.push(cleanDomain(requests[iii].initiator));
+        }
+    }
+    return domains;
+
+}
+
+function cleanDomain(domain) {
+    return domain!=undefined?domain.match(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/g)[0]:"";
 }
